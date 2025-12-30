d146cf0897c8a1c83e75734606a3dc9fef79f570
zsu58 <zsu58@icloud.com>
2025-12-30 14:00:40 +0900

    feat: add config

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
+import yaml
+
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
 
 
-load_dotenv()
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-client = TelegramClient("my_session", API_ID, API_HASH)
+def load_config():
+    with open("config.yaml", "r", encoding="utf-8") as f:
+        return yaml.safe_load(f)
 
 
-@client.on(events.NewMessage)
+# @client.on(events.NewMessage)
 async def handler(event):
     # Get details about the chat and the sender
     chat = await event.get_chat()
@@ -21,11 +21,22 @@ async def handler(event):
     username = sender.username if sender and hasattr(sender, "username") else "Unknown"
 
     # Print the message to the console
+    # TODO: filter
+    # TODO: save to DB
     print(f"[{chat_title}] @{username}")
     print(event.text)
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    config = load_config()
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    client = TelegramClient("my_session", API_ID, API_HASH)
+
+    # Register the handler manually instead of the decorator(@client.on(events.NewMessage))
+    client.add_event_handler(handler, events.NewMessage)
+
     print("Client is starting... (Check terminal for login instructions if first time)")
     client.start()
     client.run_until_disconnected()
